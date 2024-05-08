2d32aefd74d08ade53d4ae3b5afa5fb8a47cb77f
Malvika <prasaddmalvika@gmail.com>
2024-05-08 16:45:37 +0530

    java practice questions

---
+++ ReverseStringForloop.java
@@ -0,0 +1,9 @@
+public class ReverseStringForloop {
+    public static void main(String[] args) {
+        String str3="Malvika";
+        for(int i=str3.length()-1; i>=0;i--)
+        {
+            System.out.print(str3.charAt(i));
+        }
+    }
+}
---
+++ ReverseString_StringBuilder.java
@@ -0,0 +1,9 @@
+public class ReverseString_StringBuilder {
+    public static void main(String[] args) {
+        String str1="Malvika";
+        StringBuilder str2= new StringBuilder(str1);
+        str1= str2.reverse().toString();
+        System.out.println(str1);
+
+    }
+}
